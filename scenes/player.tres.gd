36ad2166d638aa24744b686da26b52d47a6d93b1
CaveJohnson376 <cavejohnson376@gmail.com>
2020-09-03 01:09:04 +0500

    preparing this project for something big pt IV

--- scenes/player.tres.gd
+++ scenes/player.tres.gd
@@ -1,21 +1,28 @@
 extends KinematicBody
 
 const gravity = 98/2       # gravity accel
-const sprintspeed = 50   # speed when sprinting
-const defspeed = 25       # speed when walking
-const accel = 10          # acceleration
-var speed = 250            # current speed
+const sprintspeed = 15     # speed when sprinting
+const defspeed = 10        # speed when walking
+const accel = 10           # acceleration
+var speed = 0              # current speed
 const jumpspd = 17         # speed of jump. Jump height depends on this
 const sensetivity = -0.005 # mouse input sensetivity
 var velocity = Vector3(0, 0, 1)   # player character movement stuff
 var mousemotion            # mouse motion
 var tile = 0               # result of copying code from other project [LEGACY]
 var isflying = false       # flying (for debug reasons)
+var cratescene = preload("res://models/cubeblock_temp/cubeblock_crate.tscn")
+var crate
 
 # don't touch this
 onready var cam = $offset/primarycam
+onready var vecconv = $vectorconvertor
 
 func _ready():
+	# create crate preview
+	crate = cratescene.instance()
+	$"../builds".add_child(crate)
+	
 	# make cursor captured
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	pass
@@ -37,6 +44,25 @@ func _process(_delta):
 		isflying = not isflying
 
 func _physics_process(delta):
+	# building preview
+	var raypos = $offset/primarycam/placeablock.get_collision_point()
+	var norm = $offset/primarycam/placeablock.get_collision_normal()
+	var dir = -$offset.global_transform.basis.y
+	crate.translation = raypos
+	crate.look_at(raypos + norm, raypos + dir)
+	
+	# building and deconstruct
+	if Input.is_action_just_pressed("place"):
+		crate.get_node("collider").disabled = false
+		crate.add_to_group("block")
+		crate = cratescene.instance()
+		$"../builds".add_child(crate)
+	if Input.is_action_just_pressed("remove"):
+		var crate = $offset/primarycam/placeablock.get_collider()
+		if crate and crate.is_in_group("block"):
+			crate.queue_free()
+		pass
+	
 	# don't touch that
 	var pos = translation
 	var up = pos.normalized()
@@ -52,24 +78,24 @@ func _physics_process(delta):
 	speed = (sprintspeed if Input.is_action_pressed("speedup") else defspeed)*scale.x
 	
 	# convert velocity to local space
-	velocity = $vectorconvertor.vector_global_to_local(Transform($offset.global_transform.basis, Vector3()), velocity)
+	velocity = vecconv.vector_global_to_local(Transform($offset.global_transform.basis, Vector3()), velocity)
 	
 	# apply vertical forces (and move vertical movement to separate var)
 	var vert_vel = velocity.z + jumpspd * int(Input.is_action_pressed("jump")) * (0.1 if isflying else int(is_on_floor())) - gravity * delta
 	velocity.z = 0
 	
-	# apply horizontal forces (not perfect solution. TODO: remake and polish this)
+	# apply horizontal forces (not perfect solution. TODO: moar polish and fix bunnyhop)
 	var move = Vector3()
 	move.y = -int(Input.is_action_pressed("backward"))+int(Input.is_action_pressed("forward"))
 	move.x = int(Input.is_action_pressed("straferight"))-int(Input.is_action_pressed("strafeleft"))
 	move = move.normalized()
-	
 	velocity = velocity.linear_interpolate(move * speed, accel * delta) if is_on_floor() else (velocity + (move * speed * delta))
 	
+	# restore vertical movement variable
 	velocity.z = vert_vel
 	
 	# convert velocity back to global space
-	velocity = $vectorconvertor.vector_local_to_global(Transform($offset.global_transform.basis, Vector3()), velocity)
+	velocity = vecconv.vector_local_to_global(Transform($offset.global_transform.basis, Vector3()), velocity)
 	
 	# apply velocity to player character
 	velocity = move_and_slide(velocity, up, true)
